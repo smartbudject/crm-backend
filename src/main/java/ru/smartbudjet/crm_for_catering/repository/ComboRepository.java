afd60630d849b07528a4a3befe4d31ee2b8630e0
vshabaev <vl7shab@yandex.ru>
2025-02-07 07:30:25 +0300

    fix error.

--- src/main/java/ru/smartbudjet/crm_for_catering/repository/ComboRepository.java
+++ src/main/java/ru/smartbudjet/crm_for_catering/repository/ComboRepository.java
@@ -3,8 +3,10 @@ package ru.smartbudjet.crm_for_catering.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ru.smartbudjet.crm_for_catering.model.entity.Combo;
+
 
 @Repository
-public interface ComboRepository extends JpaRepository<ComboRepository, Integer> {
+public interface ComboRepository extends JpaRepository<Combo, Integer> {
 
 }
--- src/main/java/ru/smartbudjet/crm_for_catering/repository/DiscountRepository.java
+++ src/main/java/ru/smartbudjet/crm_for_catering/repository/DiscountRepository.java
@@ -3,8 +3,10 @@ package ru.smartbudjet.crm_for_catering.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import ru.smartbudjet.crm_for_catering.model.entity.Discount;
+
 
 @Repository
-public interface DiscountRepository extends JpaRepository<DiscountRepository, Long> {
+public interface DiscountRepository extends JpaRepository<Discount, Long> {
 
 }
