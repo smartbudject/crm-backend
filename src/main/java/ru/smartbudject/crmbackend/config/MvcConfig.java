3df952780654c5accd79a9b34f46dbba48dfac03
Retyre535 <114016834+Retyre535@users.noreply.github.com>
2025-05-30 20:01:59 +0300

    Add files via upload
    Config

---
+++ src/main/java/ru/smartbudject/crmbackend/config/MvcConfig.java
@@ -0,0 +1,42 @@
+package ru.smartbudject.crmbackend.config;
+
+import ch.qos.logback.core.util.StringUtil;
+import jakarta.annotation.PostConstruct;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+@Configuration
+@Setter
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Value("${upload.path}")
+    private String uploadFilesPath;
+
+    @PostConstruct
+    public void init(){
+        File file = new File(uploadFilesPath);
+        if(StringUtil.isNullOrEmpty(uploadFilesPath) || (file.exists() && !file.isDirectory()))
+            throw new RuntimeException(String.format("Check upload.path property (%s), its empty or not directory", uploadFilesPath));
+        if(!(file.exists() || file.mkdirs())) throw new RuntimeException("Could not make dir " + uploadFilesPath) ;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+
+        registry
+                .addResourceHandler("/upload/**")
+                .addResourceLocations("file:/" + uploadFilesPath + "/")
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+    }
+}
